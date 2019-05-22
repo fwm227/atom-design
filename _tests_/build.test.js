ed4627432ca36b4fcd7598a73f5670c86e5bc39e
reming <927627286@qq.com>
2019-05-22 17:17:11 +0800

    :white_check_mark: fix jest test

--- _tests_/build.test.js
+++ _tests_/build.test.js
@@ -66,27 +66,51 @@ const componentNames = [
 
 describe('atom-design test', () => {
   test('build test', () => {
+    // Inject component, popup object and directive into Vue object
     Vue.use(atomD);
     const components = Vue.options.components;
+    const directives = Vue.options.directives;
     // judge component whether exist
     componentNames.forEach((compName, index) => {
       expect(components[compName]).not.toBeUndefined();
     });
+    // judge popup obj whether exist
     expect(Vue.$toast).not.toBeUndefined();
     expect(Vue.$picker).not.toBeUndefined();
     expect(Vue.$dialog).not.toBeUndefined();
     expect(Vue.$selectBox).not.toBeUndefined();
     expect(Vue.$actionSheet).not.toBeUndefined();
-    console.log(Vue)
+    // judge directive whether exist
+    expect(directives.loadmore).not.toBeUndefined();
+    expect(directives.lazyload).not.toBeUndefined();
   });
   test('component test', () => {
+    // Inject component, popup object and directive into Vue object
     compList.forEach((component, index) => {
       Vue.component(component.name, component);
     });
+    Vue.$toast = Toast;
+    Vue.$picker = Picker;
+    Vue.$dialog = Dialog;
+    Vue.$selectBox = SelectBox;
+    Vue.$actionSheet = ActionSheet;
+    Vue.directive('loadmore', LoadMore.loadmore);
+    Vue.directive('lazyload', LazyLoad.lazyload);
+
     const components = Vue.options.components;
+    const directives = Vue.options.directives;
     // judge component whether exist
     componentNames.forEach((compName, index) => {
       expect(components[compName]).not.toBeUndefined();
     });
+    // judge popup obj whether exist
+    expect(Vue.$toast).not.toBeUndefined();
+    expect(Vue.$picker).not.toBeUndefined();
+    expect(Vue.$dialog).not.toBeUndefined();
+    expect(Vue.$selectBox).not.toBeUndefined();
+    expect(Vue.$actionSheet).not.toBeUndefined();
+    // judge directive whether exist
+    expect(directives.loadmore).not.toBeUndefined();
+    expect(directives.lazyload).not.toBeUndefined();
   });
 });
