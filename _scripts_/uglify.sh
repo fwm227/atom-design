c98c1bf21e4a7c42d33bc0223974a7946a39653a
reming <927627286@qq.com>
2019-06-12 20:28:49 +0800

    :wrench: Add shell for uglify es5 code

--- _scripts_/uglify.sh
+++ _scripts_/uglify.sh
@@ -1,3 +1,60 @@
-#!/bin/bash
+uglifyjs ../lib_unuglify/actionSheet/actionSheet.js -o ../lib/actionSheet/actionSheet.js -c -m
+uglifyjs ../lib_unuglify/actionSheet/index.js -o ../lib/actionSheet/index.js -c -m
 
-uglifyjs lib_unuglify/actionSheet/index.js -o lib/actionSheet/index.js -c -m
+uglifyjs ../lib_unuglify/badge/index.js -o ../lib/badge/index.js -c -m
+
+uglifyjs ../lib_unuglify/button/index.js -o ../lib/button/index.js -c -m
+
+uglifyjs ../lib_unuglify/carousel/index.js -o ../lib/carousel/index.js -c -m
+
+uglifyjs ../lib_unuglify/common/icon/clearIcon/index.js -o ../lib/common/icon/clearIcon/index.js -c -m
+uglifyjs ../lib_unuglify/common/mask/index.js -o ../lib/common/mask/index.js -c -m
+uglifyjs ../lib_unuglify/common/mask/mask.js -o ../lib/common/mask/mask.js -c -m
+uglifyjs ../lib_unuglify/common/mixin/popup/index.js -o ../lib/common/mixin/popup/index.js -c -m
+uglifyjs ../lib_unuglify/common/mixin/popup/manager.js -o ../lib/common/mixin/popup/manager.js -c -m
+uglifyjs ../lib_unuglify/common/transition/index.js -o ../lib/common/transition/index.js -c -m
+uglifyjs ../lib_unuglify/common/util/Time.js -o ../lib/common/util/Time.js -c -m
+uglifyjs ../lib_unuglify/common/util/typeEqual.js -o ../lib/common/util/typeEqual.js -c -m
+
+uglifyjs ../lib_unuglify/dialog/dialog.js -o ../lib/carousel/dialog.js -c -m
+uglifyjs ../lib_unuglify/dialog/index.js -o ../lib/carousel/index.js -c -m
+
+uglifyjs ../lib_unuglify/drawer/index.js -o ../lib/drawer/index.js -c -m
+
+uglifyjs ../lib_unuglify/header/index.js -o ../lib/header/index.js -c -m
+
+uglifyjs ../lib_unuglify/inputItem/index.js -o ../lib/inputItem/index.js -c -m
+
+uglifyjs ../lib_unuglify/lazyload/index.js -o ../lib/lazyload/index.js -c -m
+
+uglifyjs ../lib_unuglify/loading/index.js -o ../lib/loading/index.js -c -m
+
+uglifyjs ../lib_unuglify/loadmore/index.js -o ../lib/loadmore/index.js -c -m
+
+uglifyjs ../lib_unuglify/picker/picker.js -o ../lib/picker/picker.js -c -m
+uglifyjs ../lib_unuglify/picker/pickerColumn.js -o ../lib/picker/pickerColumn.js -c -m
+uglifyjs ../lib_unuglify/picker/index.js -o ../lib/picker/index.js -c -m
+
+uglifyjs ../lib_unuglify/progress/index.js -o ../lib/progress/index.js -c -m
+
+uglifyjs ../lib_unuglify/pull/index.js -o ../lib/pull/index.js -c -m
+
+uglifyjs ../lib_unuglify/radio/index.js -o ../lib/radio/index.js -c -m
+
+uglifyjs ../lib_unuglify/range/index.js -o ../lib/range/index.js -c -m
+
+uglifyjs ../lib_unuglify/searchBar/index.js -o ../lib/searchBar/index.js -c -m
+
+uglifyjs ../lib_unuglify/selectBox/selectBox.js -o ../lib/selectBox/selectBox.js -c -m
+uglifyjs ../lib_unuglify/selectBox/index.js -o ../lib/selectBox/index.js -c -m
+
+uglifyjs ../lib_unuglify/slideItem/index.js -o ../lib/slideItem/index.js -c -m
+
+uglifyjs ../lib_unuglify/switch/index.js -o ../lib/switch/index.js -c -m
+
+uglifyjs ../lib_unuglify/tabs/index.js -o ../lib/tabs/index.js -c -m
+
+uglifyjs ../lib_unuglify/textarea/index.js -o ../lib/textarea/index.js -c -m
+
+uglifyjs ../lib_unuglify/toast/toast.js -o ../lib/toast/toast.js -c -m
+uglifyjs ../lib_unuglify/toast/index.js -o ../lib/toast/index.js -c -m
