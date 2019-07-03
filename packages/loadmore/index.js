70123e845cfddd5e38cff348be12f1083951ba97
Reming <927627286@qq.com>
2019-07-03 12:50:21 +0800

    :pencil2: Fix code

--- packages/loadmore/index.js
+++ packages/loadmore/index.js
@@ -17,8 +17,8 @@ function throttle (fn, delay) {
   };
   return function () {
     now = Date.now();
-    if (now - preExce >= delay){
-      execute()
+    if (now - preExce >= delay) {
+      execute();
     } else {
       clearTimeout(timer);
       timer = setTimeout(function () {
